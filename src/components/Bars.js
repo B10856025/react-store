fd2cacfd8d5d10b2e892f0d5d82581b483b5cc6e
B10856025 <B10856025@mail.npust.edu.tw>
2022-10-20 17:40:07 +0800

    10/20

--- src/components/Bars.js
+++ src/components/Bars.js
@@ -8,7 +8,7 @@ const Bars = () => {
             <div className="bar">
                 <p className="title has-text-centered">Content</p>
                 <ul>
-                    <li className="bar square"><label className="label"><Link to="/login">關於我們</Link></label></li>
+                    <li className="bar square"><label className="label"><Link to="/cart">關於我們</Link></label></li>
                     <li className="bar square"><label className="label"><Link to="/login">回收衣物</Link></label></li>
                     <li className="bar square"><label className="label"><Link to="/login">合作夥伴</Link></label></li>
                 </ul>
